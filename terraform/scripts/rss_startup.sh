74c49ba30f6e2246365024078ea7e305f70d987b
Maciej Lasyk <maciej@lasyk.info>
2017-10-03 23:02:49 +0200

    RSS provisioner - part 1

--- terraform/scripts/rss_startup.sh
+++ terraform/scripts/rss_startup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+cd
 
-yum install -y epel-release ansible python-dnf libselinux-python mariadb nginx
+yum install -y epel-release ansible python-dnf libselinux-python git
 yum update -y
 
 gsutil cp gs://ml-rss-lasyk-info/rss-nginx_health_check.conf /etc/nginx/default.d/health_check.conf
 
-systemctl enable --now nginx
+git clone https://github.com/docent-net/rss.lasyk.info.git
+
+cd ansible
+
+ansible-playbook configure_server.yml
