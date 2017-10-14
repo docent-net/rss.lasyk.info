64680ad641ad68403654408443d9834cdf87f290
Maciej Lasyk <maciej@lasyk.info>
2017-10-14 21:34:49 +0200

    No need for autoscaler

--- terraform/instance_group.tf
+++ terraform/instance_group.tf
@@ -15,20 +15,3 @@ resource "google_compute_instance_group_manager" "rss-instance-group-manager" {
     initial_delay_sec = 600
   }
 }
-
-resource "google_compute_autoscaler" "rss-autoscaler" {
-  provider = "google.us-central1"
-  name   = "rss-autoscaler"
-  zone   = "${var.region}-b"
-  target = "${google_compute_instance_group_manager.rss-instance-group-manager.self_link}"
-
-  autoscaling_policy = {
-    max_replicas    = 1 // cant be more due to global IP addr assignment
-    min_replicas    = 1
-    cooldown_period = 600
-
-    cpu_utilization {
-      target = 0.8
-    }
-  }
-}
